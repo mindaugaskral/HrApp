b736caf49c0dd0ceb35ca960ec39d1339606f6dd
Mindaugas Kralikauskas <mindaugaskral@gmail.com>
2022-08-02 09:50:55 +0300

    Add project files.

---
+++ HRApp/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using HRApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Candidate> CandidatesDb { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ HRApp/Models/Candidate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRApp.Models
+{
+    public class Candidate
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100,MinimumLength = 2)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Surname { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RepeatPassword { get; set; }
+        public string Note { get; set; }
+        public DateTime ContactedFirstTime { get; set; }
+        public DateTime AvalableFrom { get; set; }
+        public string LinkedInLink { get; set; }
+        public string Technologies { get; set; }
+        public bool Avalable { get; set; }
+
+    }
+}
